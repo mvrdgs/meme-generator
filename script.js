668785b8e62e7ff8dd40da79b81acfe70ad02369
Viegas <mvrdgs@gmail.com>
2021-03-11 22:04:38 -0300

    requisito 7 :rocket:

--- script.js
+++ script.js
@@ -6,6 +6,26 @@ const imageContainer = document.querySelector('#meme-image-container');
 const fireButton = document.querySelector('#fire');
 const waterButton = document.querySelector('#water');
 const earthButton = document.querySelector('#earth');
+const meme1 = document.querySelector('#meme-1');
+const meme2 = document.querySelector('#meme-2');
+const meme3 = document.querySelector('#meme-3');
+const meme4 = document.querySelector('#meme-4');
+
+function setMeme1() {
+  memeImage.setAttribute('src', 'imgs/meme1.png');
+}
+
+function setMeme2() {
+  memeImage.setAttribute('src', 'imgs/meme2.png');
+}
+
+function setMeme3() {
+  memeImage.setAttribute('src', 'imgs/meme3.png');
+}
+
+function setMeme4() {
+  memeImage.setAttribute('src', 'imgs/meme4.png');
+}
 
 function waterBorder() {
   imageContainer.style.borderColor = 'rgb(0, 0, 255)';
@@ -50,7 +70,10 @@ function setText() {
 function getText() {
   inputText.addEventListener('keyup', setText);
 }
-
+meme1.addEventListener('click', setMeme1);
+meme2.addEventListener('click', setMeme2);
+meme3.addEventListener('click', setMeme3);
+meme4.addEventListener('click', setMeme4);
 earthButton.addEventListener('click', earthBorder);
 fireButton.addEventListener('click', fireBorder);
 waterButton.addEventListener('click', waterBorder);
